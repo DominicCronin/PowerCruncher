43fff0ab135caf528c068e8f5e79eb902a8614ec
Dominic Cronin <dominic.cronin@indivirtual.com>
2015-03-12 21:58:48 +0100

    Initial Commit

---
+++ PowerCruncher.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerCruncher
+{
+
+    public class PowerCruncher
+    {
+        public PowerCruncher()
+        {   // 0=column multiplier, 1=digit, 2=radix, 3=power
+            formatString = "{2}^{3}x{1}";
+            joinString = " + ";
+        }
+        public string formatString { get; set; }
+        public string joinString { get; set; }
+
+        public string getPowers(int input)
+        {
+            return getPowers(input, 2);
+        }
+
+        public string getPowers(int input, int radix)
+        {
+            var remainder = input;
+            int power = 0;
+            var results = new List<string>();
+            while (true) {
+                int digit = remainder % radix;
+                results.Add(string.Format(formatString, (int)Math.Pow(radix, power), digit, radix, power));                   
+                remainder -= digit;
+                remainder /= radix;
+                power++;
+
+                if (remainder <= 0) { break; }
+
+            }
+            return string.Join(this.joinString, results.Reverse<string>());
+        }
+    }
+}
---
+++ PowerCruncherTests/PowerCruncherTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerCruncher;
+using System.Diagnostics;
+
+namespace PowerCruncherTests
+{
+    [TestClass]
+    public class PowerCruncherTests
+    {
+        [TestMethod]
+        public void Test98765InBaseTenDigits()
+        {
+            var subject = new PowerCruncher.PowerCruncher();
+            subject.formatString = "{1}";
+            subject.joinString = "";
+            Assert.AreEqual("98765", subject.getPowers(98765, 10) );
+        }
+
+        [TestMethod]
+        public void Test97InBinaryDigits()
+        {
+            var subject = new PowerCruncher.PowerCruncher();
+            subject.formatString = "{1}";
+            subject.joinString = "";
+            Assert.AreEqual("1100001", subject.getPowers(97, 2));
+        }
+
+        [TestMethod]
+        public void Test43InOctalDigits()
+        {
+            var subject = new PowerCruncher.PowerCruncher();
+            subject.formatString = "{1}";
+            subject.joinString = "";
+            Assert.AreEqual("53", subject.getPowers(43, 8));
+        }
+
+        //3 = 2(0) + 2 (1) 
+        [TestMethod]
+        public void Test3InBinaryWithElenaFormatting()
+        {
+            var subject = new PowerCruncher.PowerCruncher();
+            subject.formatString = "{0}({1})";
+            Assert.AreEqual("2(1) + 1(1)", subject.getPowers(3, 2));
+        }
+
+        [TestMethod]
+        public void Test67InBinaryWithDefaultFormatting()
+        {
+            var subject = new PowerCruncher.PowerCruncher();
+            Assert.AreEqual("2^6x1 + 2^5x0 + 2^4x0 + 2^3x0 + 2^2x0 + 2^1x1 + 2^0x1", subject.getPowers(67, 2));
+        }
+
+        [TestMethod]
+        public void Test67InDecimalWithColumnMultipliedFormatting()
+        {
+            var subject = new PowerCruncher.PowerCruncher();
+            subject.formatString = "{0}x{1}";
+            Assert.AreEqual("10x6 + 1x7", subject.getPowers(67, 10));
+        }
+
+    }
+}
